a48e3fd709c8d0224eaf6695a7b51ca1f57d0f80
olegs <olegs@BFI_W10PC01>
2022-10-05 16:47:13 +0300

    abstract class implemented

--- GuildRose.Tests/ProgramTests.cs
+++ GuildRose.Tests/ProgramTests.cs
@@ -5,55 +5,131 @@ namespace GildedRose.Tests
 {
     public class ProgramTests
     {
-        [Fact]
-        public void Update_Default_Items()
+        [Theory]
+        [InlineData(10, 20, 9, 19)]
+        [InlineData(-1, 5, -2, 3)]
+        public void Update_Default_Items(int passedSellIn, int passedQuality, int expectedSellIn, int expectedQuality)
         {
             //ARRANGE
-            List<MarkedItem> markedItems = new List<MarkedItem>()
-                {
-                    new MarkedItem {
-                                    Item = new Item { Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20 }
-                                    },
-                    new MarkedItem {
-                                    isDefault = false,
-                                    isQualityIncreases = true,
-                                    ItemType = (int)ItemType.Increased,
-                                    Item = new Item { Name = "Aged Brie", SellIn = 2, Quality = 0 }
-                                    },
-                    new MarkedItem {
-                                    Item = new Item { Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7 }
-                                    },
-                    new MarkedItem {
-                                    isDefault = false,
-                                    isChangeable = false,
-                                    Item = new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80 }
-                                    },
-                    new MarkedItem {
-                                    isDefault = false,
-                                    isQualityIncreasesWithOptions = true,
-                                    ItemType = (int)ItemType.Overdue,
-                                    IncreaseOptions = new int[2]{11,6},
-                                    Item = new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 15, Quality = 20 }
-                                    },
-                    new MarkedItem {
-                                    QualityDecrement = 2,
-                                    isDefault = false,
-                                    isConjured = true,
-                                    ItemType = (int)ItemType.Conjured,
-                                    Item = new Item { Name = "Conjured Mana Cake", SellIn = 3, Quality = 6 }
-                                    }
-                };
+            List<AbstractItem> items = new List<AbstractItem>()
+            {
+                new DefaultItem(new Item {Name = "+5 Dexterity Vest", SellIn = passedSellIn, Quality = passedQuality }),
+            };
 
-            Program app = new Program(markedItems);
-
+            Program app = new Program(items);
 
             //ACT
-
-            app.UpdateDefault(app.markedItems[0]);
+            app.UpdateQuality();
 
             //ASSERT
-
-            Assert.Equal(9, app.markedItems[0].Item.SellIn);
+            Assert.Equal(expectedSellIn, items[0].Item.SellIn);
+            Assert.Equal(expectedQuality, items[0].Item.Quality);
         }
+
+        [Theory]
+        [InlineData(-1, -1)]
+        [InlineData(10, 52)]
+        public void Update_Default_Items_Throws(int passedSellIn, int passedQuality)
+        {
+            //ARRANGE
+            List<AbstractItem> items = new List<AbstractItem>()
+            {
+                new DefaultItem(new Item {Name = "+5 Dexterity Vest", SellIn = passedSellIn, Quality = passedQuality }),
+            };
+
+            Program app = new Program(items);
+
+            //ACT
+            Assert.Throws<Exception>(() => app.UpdateQuality());
+        }
+
+        //    [Fact]
+        //    public void Update_Conjured_Items()
+        //    {
+        //        //ARRANGE
+        //        List<AbstractItem> items = new List<AbstractItem>()
+        //        {
+        //            new ConjuredItem(new Item { Name = "Conjured Mana Cake", SellIn = 3, Quality = 6 }),
+        //        };
+
+        //        Program app = new Program(items);
+
+
+        //        //ACT
+
+        //        app.UpdateQuality();
+
+        //        //ASSERT
+
+        //        Assert.Equal(-2, items[0].Item.SellIn);
+        //        Assert.Equal(18, items[0].Item.Quality);
+        //    }
+
+        //    [Fact]
+        //    public void Update_Increased_Items()
+        //    {
+        //        //ARRANGE
+        //        List<AbstractItem> items = new List<AbstractItem>()
+        //        {
+        //            new IncreasedItem(new Item { Name = "Aged Brie", SellIn = 2, Quality = 0 }),
+        //        };
+
+        //        Program app = new Program(items);
+
+
+        //        //ACT
+
+        //        app.UpdateQuality();
+
+        //        //ASSERT
+
+        //        Assert.Equal(-2, items[0].Item.SellIn);
+        //        Assert.Equal(18, items[0].Item.Quality);
+        //    }
+
+        //    [Fact]
+        //    public void Update_IncreasedWithOptions_Items()
+        //    {
+        //        //ARRANGE
+        //        List<AbstractItem> items = new List<AbstractItem>()
+        //        {
+        //            new IncreasedWithOptionsItem(new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80 }, new int[2]{11, 3}),
+        //        };
+
+        //        Program app = new Program(items);
+
+
+        //        //ACT
+
+        //        app.UpdateQuality();
+
+        //        //ASSERT
+
+        //        Assert.Equal(-2, items[0].Item.SellIn);
+        //        Assert.Equal(18, items[0].Item.Quality);
+        //    }
+
+        //    [Fact]
+        //    public void Update_Legendary_Items()
+        //    {
+        //        //ARRANGE
+        //        List<AbstractItem> items = new List<AbstractItem>()
+        //        {
+        //            new LegendaryItem(new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80 })
+        //        };
+
+        //        Program app = new Program(items);
+
+
+        //        //ACT
+
+        //        app.UpdateQuality();
+
+        //        //ASSERT
+
+        //        Assert.Equal(-2, items[0].Item.SellIn);
+        //        Assert.Equal(18, items[0].Item.Quality);
+        //    }
+        //}
     }
 }
--- src/GildedRose.Console/Program.cs
+++ src/GildedRose.Console/Program.cs
@@ -6,203 +6,280 @@ namespace GildedRose.Console
 {
     public class Program
     {
-        public List<MarkedItem> markedItems;
+        public List<AbstractItem> Items;
 
-        public Program(List<MarkedItem> markedItems)
+        public Program(List<AbstractItem> items)
         {
-            this.markedItems = markedItems;
+            Items = items;
         }
 
         static void Main(string[] args)
         {
-            System.Console.WriteLine("OMGHAI!");
+            List<AbstractItem> items = new List<AbstractItem>()
+            {
+                new DefaultItem(new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20 }),
+                new ConjuredItem(new Item { Name = "Conjured Mana Cake", SellIn = 3, Quality = 6 }),
+                new IncreasedItem(new Item { Name = "Aged Brie", SellIn = 2, Quality = 0 }),
+                new IncreasedWithOptionsItem(new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80 }, new int[2]{11, 3}),
+                new LegendaryItem(new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80 })
+            };
 
-            List<MarkedItem> markedItems = new List<MarkedItem>()
-                {
-                    new MarkedItem {
-                                    Item = new Item { Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20 }
-                                    },
-                    new MarkedItem {
-                                    isDefault = false,
-                                    isQualityIncreases = true,
-                                    ItemType = (int)ItemType.Increased,
-                                    Item = new Item { Name = "Aged Brie", SellIn = 2, Quality = 0 }
-                                    },
-                    new MarkedItem {
-                                    Item = new Item { Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7 }
-                                    },
-                    new MarkedItem {
-                                    isDefault = false,
-                                    isChangeable = false,
-                                    Item = new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80 }
-                                    },
-                    new MarkedItem {
-                                    isDefault = false,
-                                    isQualityIncreasesWithOptions = true,
-                                    ItemType = (int)ItemType.Overdue,
-                                    IncreaseOptions = new int[2]{11,6},
-                                    Item = new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 15, Quality = 20 }
-                                    },
-                    new MarkedItem {
-                                    QualityDecrement = 2,
-                                    isDefault = false,
-                                    isConjured = true,
-                                    ItemType = (int)ItemType.Conjured,
-                                    Item = new Item { Name = "Conjured Mana Cake", SellIn = 3, Quality = 6 }
-                                    }
-                };
-
-            var app = new Program(markedItems);
-            
+            var app = new Program(items);
 
             app.UpdateQuality();
             System.Console.ReadKey();
-
-        }
-
-
-        public void CheckForOverdue(MarkedItem item, int type)
-        {
-            switch (type)
-            {
-                case (int)ItemType.Default:
-                    if (item.Item.SellIn < MarkedItem.sellInDefault && item.Item.Quality > MarkedItem.minQuality)
-                    {
-                        item.Item.Quality = item.Item.Quality - item.QualityDecrement;
-                    }
-                    break;
-                case (int)ItemType.Overdue:
-                    if (item.Item.SellIn < MarkedItem.sellInDefault && item.Item.Quality < MarkedItem.maxQuality)
-                    {
-                        item.Item.Quality = item.Item.Quality - item.Item.Quality;
-                    }
-                    break;
-                case (int)ItemType.Conjured:
-                    if (item.Item.SellIn < MarkedItem.sellInDefault && item.Item.Quality > MarkedItem.ConjuredMinQualityValue)
-                    {
-                        item.Item.Quality = item.Item.Quality - item.QualityDecrement;
-                    }
-                    break;
-                case (int)ItemType.Increased:
-                    if (item.Item.SellIn < MarkedItem.sellInDefault && item.Item.Quality < MarkedItem.maxQuality)
-                    {
-                        item.Item.Quality = item.Item.Quality + item.QualityIncrement;
-                    }
-                    break;
-            }
-
-        }
-        public void UpdateDefault(MarkedItem item)
-        {
-            if (item.isDefault && item.Item.Quality > MarkedItem.minQuality)
-            {
-                item.Item.Quality = item.Item.Quality - item.QualityDecrement;
-                item.Item.SellIn = item.Item.SellIn - item.SellInDecrement;
-            }
-        }
-
-        public void UpdateConjured(MarkedItem item)
-        {
-            if (item.isConjured && item.Item.Quality > MarkedItem.ConjuredMinQualityValue)
-            {
-                item.Item.Quality = item.Item.Quality - item.QualityDecrement;
-                item.Item.SellIn = item.Item.SellIn - item.SellInDecrement;
-            }
-        }
-
-        public void UpdateIncreased(MarkedItem item)
-        {
-            if (item.isQualityIncreases && item.Item.Quality < MarkedItem.maxQuality)
-            {
-                item.Item.Quality = item.Item.Quality + item.QualityIncrement;
-                item.Item.SellIn = item.Item.SellIn - item.SellInDecrement;
-            }
-
-        }
-
-        public void UpdateIncreasedWithOptions(MarkedItem item)
-        {
-            if (item.isQualityIncreasesWithOptions && item.IncreaseOptions != null && item.Item.Quality < MarkedItem.maxQuality)
-            {
-                item.Item.Quality = item.Item.Quality + item.QualityIncrement;
-                item.Item.SellIn = item.Item.SellIn - item.SellInDecrement;
-
-                if (item.Item.Quality < MarkedItem.maxQuality)
-                {
-                    item.Item.Quality = item.Item.Quality + item.QualityIncrement;
-
-                    for (int i = 0; i < item.IncreaseOptions.Count(); i++)
-                    {
-                        if (item.Item.SellIn < item.IncreaseOptions[i])
-                        {
-                            if (item.Item.Quality < MarkedItem.maxQuality)
-                            {
-                                item.Item.Quality = item.Item.Quality + item.QualityIncrement;
-                            }
-                        }
-                    }
-                }
-            }
-
         }
 
         public void UpdateQuality()
         {
 
-            for (var i = 0; i < markedItems.Count; i++)
+            foreach (var item in Items)
             {
-                if (markedItems[i].isChangeable)
-                {
-                    UpdateDefault(markedItems[i]);
-                    UpdateConjured(markedItems[i]);
-                    UpdateIncreased(markedItems[i]);
-                    UpdateIncreasedWithOptions(markedItems[i]);
-
-                    CheckForOverdue(markedItems[i], markedItems[i].ItemType);
-                }
+                item.Update();
+                item.UpdateIfOverdue();
             }
         }
-        public enum ItemType : int
+
+        public enum SellInCheckTypeEnum : int
         {
             Default = 0,
             Overdue = 1,
             Conjured = 2,
             Increased = 3
         }
-        public class MarkedItem
+        public abstract class AbstractItem
         {
-            /// <summary>
-            /// maxValue = 50
-            /// </summary>
-            public static int maxQuality = 50;
-            /// <summary>
-            /// minValue = 0
-            /// </summary>
-            public static int minQuality = 0;
-            /// <summary>
-            /// minValue = 2
-            /// </summary>
-            public static int ConjuredMinQualityValue = 1;
-            /// <summary>
-            /// value = 0
-            /// </summary>
-            public static int sellInDefault = 0;
-            public bool isDefault { get; set; } = true;
-            public bool isChangeable { get; set; } = true;
-            public bool isConjured { get; set; } = false;
-            public bool isQualityIncreases { get; set; } = false;
-            public bool isQualityIncreasesWithOptions { get; set; } = false;
-            public int[] IncreaseOptions { get; set; }
-            public int ItemType { get; set; } = 0;
-            public int QualityDecrement { get; set; } = 1;
-            public int QualityIncrement { get; set; } = 1;
-            public int SellInDecrement { get; set; } = 1;
-            public int SellInIncrement { get; set; } = 1;
             public Item Item;
+            public abstract int MaxQuality { get;}
+            public abstract int MinQuality { get;}
+            public abstract int SellInDefault { get;}
+            public abstract  SellInCheckTypeEnum SellInType { get;}
+            public abstract int QualityDecrement { get;}
+            public abstract int QualityIncrement { get;}
+            public abstract int SellInDecrement { get;}
+            public abstract int SellInIncrement { get;}
+            public AbstractItem(Item item)
+            {
+                Item = item;
+            }
 
+            public abstract void Update();
+
+            public void UpdateIfOverdue()
+            {
+                switch (SellInType)
+                {
+                    case SellInCheckTypeEnum.Default:
+                        if (Item.SellIn < SellInDefault && Item.Quality > MinQuality)
+                        {
+                            Item.Quality = Item.Quality - QualityDecrement;
+                        }
+                        break;
+                    case SellInCheckTypeEnum.Overdue:
+                        if (Item.SellIn < SellInDefault && Item.Quality < MaxQuality)
+                        {
+                            Item.Quality = Item.Quality - Item.Quality;
+                        }
+                        break;
+                    case SellInCheckTypeEnum.Conjured:
+                        if (Item.SellIn < SellInDefault && Item.Quality > MinQuality)
+                        {
+                            Item.Quality = Item.Quality - QualityDecrement;
+                        }
+                        break;
+                    case SellInCheckTypeEnum.Increased:
+                        if (Item.SellIn < SellInDefault && Item.Quality < MaxQuality)
+                        {
+                            Item.Quality = Item.Quality + QualityIncrement;
+                        }
+                        break;
+                }
+            }
+            
+        }
+        public class DefaultItem : AbstractItem
+        {
+
+            public override int MaxQuality { get; }
+            public override int MinQuality { get; }
+            public override int SellInDefault { get; }
+            public override SellInCheckTypeEnum SellInType { get; }
+            public override int QualityDecrement { get; }
+            public override int QualityIncrement { get; }
+            public override int SellInDecrement { get; }
+            public override int SellInIncrement { get; }
+            public DefaultItem(Item item) : base(item)
+            {
+                MaxQuality = 50;
+                MinQuality = 0;
+                SellInDefault = 0;
+                SellInType = SellInCheckTypeEnum.Default;
+                QualityDecrement = 1;
+                QualityIncrement = 1;
+                SellInDecrement = 1;
+                SellInIncrement = 1;
+            }
+            public override void Update()
+            {
+                if (Item.Quality > MaxQuality) throw new Exception($"Item Quality could not be greater than {MaxQuality}");
+
+                if (Item.Quality > MinQuality)
+                {
+                    Item.Quality = Item.Quality - QualityDecrement;
+                    Item.SellIn = Item.SellIn - SellInDecrement;
+                }
+                else
+                {
+                    throw new Exception($"Item Quality could not be less than {MinQuality}");
+                }
+            }
+        }
+        public class ConjuredItem : AbstractItem
+        {
+            public override int MaxQuality { get;}
+            public override int MinQuality { get; }
+            public override int SellInDefault { get;}
+            public override SellInCheckTypeEnum SellInType { get; }
+            public override int QualityDecrement { get;}
+            public override int QualityIncrement { get;}
+            public override int SellInDecrement { get;}
+            public override int SellInIncrement { get;}
+
+            public ConjuredItem(Item item) : base(item)
+            {
+                MaxQuality = 50;
+                MinQuality = 1;
+                SellInDefault = 0;
+                SellInType = SellInCheckTypeEnum.Conjured;
+                QualityDecrement = 2;
+                QualityIncrement = 1;
+                SellInDecrement = 1;
+                SellInIncrement = 1;
+            }
+            public override void Update()
+            {
+                if (Item.Quality > MaxQuality) throw new Exception($"Item Quality could not be greater than {MaxQuality}");
+
+                if (Item.Quality > MinQuality)
+                {
+                    Item.Quality = Item.Quality - QualityDecrement;
+                    Item.SellIn = Item.SellIn - SellInDecrement;
+                }
+                else
+                {
+                    throw new Exception($"Item Quality could not be less than {MinQuality}");
+                }
+            }
+            
         }
 
-        
+        public class IncreasedItem : AbstractItem
+        {
+            public override int MaxQuality { get; }
+            public override int MinQuality { get; }
+            public override int SellInDefault { get; }
+            public override SellInCheckTypeEnum SellInType { get; }
+            public override int QualityDecrement { get; }
+            public override int QualityIncrement { get; }
+            public override int SellInDecrement { get; }
+            public override int SellInIncrement { get; }
+
+            public IncreasedItem(Item item) : base(item)
+            {
+                MaxQuality = 50;
+                MinQuality = 0;
+                SellInDefault = 0;
+                SellInType = SellInCheckTypeEnum.Increased;
+                QualityDecrement = 1;
+                QualityIncrement = 1;
+                SellInDecrement = 1;
+                SellInIncrement = 1;
+            }
+            public override void Update()
+            {
+                if (Item.Quality < MinQuality) throw new Exception($"Item Quality could not be less than {MinQuality}");
+
+                if (Item.Quality < MaxQuality)
+                {
+                    Item.Quality = Item.Quality + QualityIncrement;
+                    Item.SellIn = Item.SellIn - SellInDecrement;
+                }
+                else
+                {
+                   throw new Exception($"Item Quality could not be greater than {MaxQuality}");
+                }
+            }
+        }
+
+        public class IncreasedWithOptionsItem : AbstractItem
+        {
+            public override int MaxQuality { get; }
+            public override int MinQuality { get; }
+            public override int SellInDefault { get; }
+            public override SellInCheckTypeEnum SellInType { get; }
+            public override int QualityDecrement { get; }
+            public override int QualityIncrement { get; }
+            public override int SellInDecrement { get; }
+            public override int SellInIncrement { get; }
+            /// <summary>
+            /// SellIn options
+            /// </summary>
+            public int[] Options { get; }
+            public IncreasedWithOptionsItem(Item item, int[] options) : base(item)
+            {
+                Options = options;
+                MaxQuality = 50;
+                MinQuality = 0;
+                SellInDefault = 0;
+                SellInType = SellInCheckTypeEnum.Overdue;
+                QualityDecrement = 1;
+                QualityIncrement = 1;
+                SellInDecrement = 1;
+                SellInIncrement = 1;
+            }
+            public override void Update()
+            {
+                if (Item.Quality < MinQuality) throw new Exception($"Item Quality could not be less than {MinQuality}");
+
+                if (Item.Quality < MaxQuality)
+                {
+                    Item.Quality = Item.Quality + QualityIncrement;
+                    Item.SellIn = Item.SellIn - SellInDecrement;
+
+                    for (int i = 0; i < Options.Count(); i++)
+                    {
+                        if (Item.SellIn < Options[i])
+                        {
+                            if (Item.Quality < MaxQuality)
+                            {
+                                Item.Quality = Item.Quality + QualityIncrement;
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    throw new Exception($"Item Quality could not be greater than {MaxQuality}");
+                }
+            }
+            
+        }
+
+        public class LegendaryItem : AbstractItem
+        {
+            public override int MaxQuality { get; }
+            public override int MinQuality { get; }
+            public override int SellInDefault { get; }
+            public override SellInCheckTypeEnum SellInType { get; }
+            public override int QualityDecrement { get; }
+            public override int QualityIncrement { get; }
+            public override int SellInDecrement { get; }
+            public override int SellInIncrement { get; }
+
+            public LegendaryItem(Item item) : base(item) { }
+            
+            public override void Update() { }
+        }
     }
     public class Item
     {
